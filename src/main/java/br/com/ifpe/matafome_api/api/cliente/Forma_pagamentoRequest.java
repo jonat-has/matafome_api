1fb2a4c8cc1a216751ef8c11c1d821a207fe5aa3
jonat-has <116895496+jonat-has@users.noreply.github.com>
2024-08-30 10:01:37 -0300

    Bug de registrar cliente

--- src/main/java/br/com/ifpe/matafome_api/api/cliente/Forma_pagamentoRequest.java
+++ src/main/java/br/com/ifpe/matafome_api/api/cliente/Forma_pagamentoRequest.java
@@ -16,8 +16,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Forma_pagamentoRequest {
 
-    @NotNull(message = "O Tipo de pagamento é de preenchimento obrigatório")
-    @NotBlank(message = "O Tipo de pagamento não pode ser vazio")
     private String tipo;
 
     @Pattern(regexp = "\\d{16}", message = "O Número do cartão deve ter 16 dígitos")
@@ -26,20 +24,13 @@ public class Forma_pagamentoRequest {
     @Pattern(regexp = "(0[1-9]|1[0-2])/\\d{2}", message = "A Data de validade deve estar no formato MM/AA")
     private String data_validade;
 
-    @NotNull(message = "O Nome do titular é de preenchimento obrigatório")
-    @NotBlank(message = "O Nome do titular não pode ser vazio")
     @Size(max = 100, message = "O Nome do titular deve ter no máximo {max} caracteres")
     private String nome_titular;
 
-    @NotNull(message = "O CVV é de preenchimento obrigatório")
     private Integer cvv;
 
-    @NotNull(message = "O Endereço de cobrança é de preenchimento obrigatório")
-    @NotBlank(message = "O Endereço de cobrança não pode ser vazio")
     private String endereco_cobranca;
 
-    @NotNull(message = "A Cidade de cobrança é de preenchimento obrigatório")
-    @NotBlank(message = "A Cidade de cobrança não pode ser vazia")
     private String cidade_cobranca;
 
     @Pattern(regexp = "[A-Z]{2}", message = "O Estado de cobrança deve ser composto por duas letras maiúsculas")
